903e7e9b9293561913e61521c52f48c66b9cdfe8
Kabeer <Kabeer@192.168.1.3>
2017-02-16 22:05:04 +0530

    commit

---
+++ src/main/java/com/revature/model/Book.java
@@ -0,0 +1,31 @@
+package com.revature.model;
+
+public class Book {
+	private String id;
+	private String bookName;
+	private String price;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getBookName() {
+		return bookName;
+	}
+
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+}
---
+++ src/test/java/com/revature/model/TestBook.java
@@ -0,0 +1,11 @@
+package com.revature.model;
+
+public class TestBook {
+	public static void main(String[] args) {
+
+		Book book = new Book();
+		book.setId("001");
+		book.setBookName("wings of fire");
+		book.setPrice("250");
+	}
+}
